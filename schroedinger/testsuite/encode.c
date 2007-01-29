02709d2cf6e639b4ff7e00689e7efd1bcb58128f
David Schleef <ds@schleef.org>
2007-01-29 21:23:50 +0000

    r219: * testsuite/encode.c: Use improved schro_encoder_iterate() API.

--- schroedinger/testsuite/encode.c
+++ schroedinger/testsuite/encode.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <schroedinger/schro.h>
+#include <schroedinger/schrodebug.h>
 
 #define ROUND_UP_2(x) (((x) + 1) & ~1)
 #define ROUND_UP_4(x) (((x) + 3) & ~3)
@@ -22,13 +23,14 @@ frame_free (SchroFrame *frame, void *priv)
 void
 test (int w, int h)
 {
-  int i;
   int size;
   uint8_t *picture;
   SchroEncoder *encoder;
   SchroBuffer *buffer;
   SchroFrame *frame;
   SchroVideoFormat *format;
+  int n_frames;
+  int go;
 
   encoder = schro_encoder_new();
   format = schro_encoder_get_video_format(encoder);
@@ -40,32 +42,45 @@ test (int w, int h)
   size += (ROUND_UP_8 (w)/2) * (ROUND_UP_2 (h)/2);
   size += (ROUND_UP_8 (w)/2) * (ROUND_UP_2 (h)/2);
 
-  for(i=0;i<10000;i++){
+  n_frames = 0;
+  go = 1;
+  while (go) {
     int x;
 
-    picture = malloc(size);
-    memset(picture, 128, size);
+    switch (schro_encoder_iterate (encoder)) {
+      case SCHRO_STATE_NEED_FRAME:
+        if (n_frames < 100) {
+          //SCHRO_ERROR("frame %d", n_frames);
 
-    frame = schro_frame_new_I420 (picture, w, h);
+          picture = malloc(size);
+          memset(picture, 128, size);
 
-    schro_frame_set_free_callback (frame, frame_free, picture);
+          frame = schro_frame_new_I420 (picture, w, h);
 
-    schro_encoder_push_frame (encoder, frame);
+          schro_frame_set_free_callback (frame, frame_free, picture);
 
-    buffer = schro_encoder_encode (encoder);
-    if (buffer) {
-      schro_buffer_unref (buffer);
+          schro_encoder_push_frame (encoder, frame);
+
+          n_frames++;
+        } else {
+          schro_encoder_end_of_stream (encoder);
+        }
+        break;
+      case SCHRO_STATE_HAVE_BUFFER:
+      case SCHRO_STATE_AGAIN:
+        break;
+      case SCHRO_STATE_END_OF_STREAM:
+        go = 0;
+        break;
+      default:
+        break;
     }
-
     buffer = schro_encoder_pull (encoder, &x);
     while (buffer) {
+      //SCHRO_ERROR("outbuf %d", buffer->length);
       schro_buffer_unref (buffer);
       buffer = schro_encoder_pull (encoder, &x);
     }
-  }
-  schro_encoder_end_of_stream (encoder);
-  while ((buffer = schro_encoder_encode (encoder))) {
-    schro_buffer_unref (buffer);
   }
 
   schro_encoder_free (encoder);
