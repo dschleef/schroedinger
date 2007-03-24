bd06d8e2bf93a6fee3c3abfbf34e5bfbba2f64cf
David Schleef <ds@schleef.org>
2007-03-24 22:27:16 +0000

    r273: * testsuite/coder/arith_exp.c: Add Thomas's new coder idea.

--- schroedinger/testsuite/coder/arith_exp.c
+++ schroedinger/testsuite/coder/arith_exp.c
@@ -9,6 +9,45 @@
 
 #include "arith.h"
 
+
+
+const unsigned int lut[256] = {
+  //LUT corresponds to window = 16 @ p0=0.5 & 256 @ p=1.0
+     0,    2,    5,    8,   11,   15,   20,   24,
+    29,   35,   41,   47,   53,   60,   67,   74,
+    82,   89,   97,  106,  114,  123,  132,  141,
+   150,  160,  170,  180,  190,  201,  211,  222,
+   233,  244,  256,  267,  279,  291,  303,  315,
+   327,  340,  353,  366,  379,  392,  405,  419,
+   433,  447,  461,  475,  489,  504,  518,  533,
+   548,  563,  578,  593,  609,  624,  640,  656,
+   672,  688,  705,  721,  738,  754,  771,  788,
+   805,  822,  840,  857,  875,  892,  910,  928,
+   946,  964,  983, 1001, 1020, 1038, 1057, 1076,
+  1095, 1114, 1133, 1153, 1172, 1192, 1211, 1231,
+  1251, 1271, 1291, 1311, 1332, 1352, 1373, 1393,
+  1414, 1435, 1456, 1477, 1498, 1520, 1541, 1562,
+  1584, 1606, 1628, 1649, 1671, 1694, 1716, 1738,
+  1760, 1783, 1806, 1828, 1851, 1874, 1897, 1920,
+  1935, 1942, 1949, 1955, 1961, 1968, 1974, 1980,
+  1985, 1991, 1996, 2001, 2006, 2011, 2016, 2021,
+  2025, 2029, 2033, 2037, 2040, 2044, 2047, 2050,
+  2053, 2056, 2058, 2061, 2063, 2065, 2066, 2068,
+  2069, 2070, 2071, 2072, 2072, 2072, 2072, 2072,
+  2072, 2071, 2070, 2069, 2068, 2066, 2065, 2063,
+  2060, 2058, 2055, 2052, 2049, 2045, 2042, 2038,
+  2033, 2029, 2024, 2019, 2013, 2008, 2002, 1996,
+  1989, 1982, 1975, 1968, 1960, 1952, 1943, 1934,
+  1925, 1916, 1906, 1896, 1885, 1874, 1863, 1851,
+  1839, 1827, 1814, 1800, 1786, 1772, 1757, 1742,
+  1727, 1710, 1694, 1676, 1659, 1640, 1622, 1602,
+  1582, 1561, 1540, 1518, 1495, 1471, 1447, 1422,
+  1396, 1369, 1341, 1312, 1282, 1251, 1219, 1186,
+  1151, 1114, 1077, 1037,  995,  952,  906,  857,
+   805,  750,  690,  625,  553,  471,  376,  255
+};
+
+#if 0
 static uint16_t division_factor[257] = {
       0,     0, 32768, 21845,
   16384, 13107, 10923,  9362,
@@ -76,6 +115,8 @@ static uint16_t division_factor[257] = {
     260,   259,   258,   257,
     256
 };
+#endif
+
 
 void
 arith_exp_init (Arith *arith)
@@ -143,8 +184,14 @@ arith_exp_encode (Arith *arith, int i, int value)
   } else {
     arith->range1 = range_x_prob;
   }
-  arith->contexts[i].count[value]++;
-  arith->contexts[i].n++;
+  //arith->contexts[i].count[value]++;
+  //arith->contexts[i].n++;
+  if (value) {
+    arith->contexts[i].probability -= lut[arith->contexts[i].probability>>8];
+  } else {
+    arith->contexts[i].probability += lut[255-(arith->contexts[i].probability>>8)];
+  }
+#if 0
   if (arith->contexts[i].n == 16) {
     unsigned int scaler;
     unsigned int weight;
@@ -160,13 +207,14 @@ arith_exp_encode (Arith *arith, int i, int value)
     scaler = division_factor[weight];
     arith->contexts[i].probability = arith->contexts[i].count[0] * scaler;
   }
+#endif
   
-#if 0
-  while (arith->range1 < 0x1000) {
-    arith->range0 <<= 2;
-    arith->range1 <<= 2;
+  while (arith->range1 < 0x8000) {
 
-    arith->cntr+=2;
+    arith->range0 <<= 1;
+    arith->range1 <<= 1;
+
+    arith->cntr++;
     if (arith->cntr == 8) {
       if (arith->range0 < (1<<24) &&
           (arith->range0 + arith->range1) >= (1<<24)) {
@@ -193,27 +241,6 @@ arith_exp_encode (Arith *arith, int i, int value)
       arith->cntr = 0;
     }
   }
-#endif
-#if 1
-  while (arith->range1 <= 0x1000) {
-
-    if (((arith->range0 + arith->range1)^arith->range0)>=0x8000) {
-      arith->range0 ^= 0x4000;
-      arith->carry++;
-    } else {
-      arith->range0 <<= 1;
-      arith->range1 <<= 1;
-
-      arith->cntr++;
-      if (arith->cntr == 8) {
-        arith->data[arith->offset] = arith->range0 >> 16;
-        arith->offset++;
-        arith->range0 &= 0xffff;
-        arith->cntr = 0;
-      }
-    }
-  }
-#endif
 }
 
 static int
@@ -238,8 +265,14 @@ arith_exp_decode (Arith *arith, int i)
   } else {
     arith->range1 = range_x_prob;
   }
-  arith->contexts[i].count[value]++;
-  arith->contexts[i].n++;
+  //arith->contexts[i].count[value]++;
+  //arith->contexts[i].n++;
+  if (value) {
+    arith->contexts[i].probability -= lut[arith->contexts[i].probability>>8];
+  } else {
+    arith->contexts[i].probability += lut[255-(arith->contexts[i].probability>>8)];
+  }
+#if 0
   if (arith->contexts[i].n == 16) {
     unsigned int scaler;
     unsigned int weight;
@@ -255,12 +288,9 @@ arith_exp_decode (Arith *arith, int i)
     scaler = division_factor[weight];
     arith->contexts[i].probability = arith->contexts[i].count[0] * scaler;
   }
+#endif
   
-  while (arith->range1 < 0x1000) {
-    if (((arith->range0 + arith->range1)^arith->range0)>=0x8000) {
-      arith->range0 ^= 0x4000;
-      arith->code ^= 0x4000;
-    }
+  while (arith->range1 < 0x8000) {
 
     arith->range0 <<= 1;
     arith->range1 <<= 1;
@@ -271,10 +301,13 @@ arith_exp_decode (Arith *arith, int i)
     arith->cntr++;
     if (arith->cntr == 8) {
       arith->offset++;
+      arith->range0 &= 0xffff;
+      arith->code &= 0xffff;
+      if (arith->code < arith->range0) {
+        arith->code |= (1<<16);
+      }
       arith->cntr = 0;
     }
-    arith->range0 &= 0xffff;
-    arith->code &= 0xffff;
   }
   return value;
 }
