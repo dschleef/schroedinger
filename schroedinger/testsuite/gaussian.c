1f075d5559cbce937f2ac657cd9c7fd6eb4156f8
David Schleef <ds@schleef.org>
2007-06-21 00:34:20 +0000

    r345: * testsuite/gaussian.c: Refactor and clean up.

--- schroedinger/testsuite/gaussian.c
+++ schroedinger/testsuite/gaussian.c
@@ -9,16 +9,19 @@
 #include <schroedinger/schro.h>
 #include <schroedinger/schrodebug.h>
 #include <schroedinger/schroutils.h>
+#include <liboil/liboilprofile.h>
 #include <math.h>
 
 #define N 2000
 #define INT 1
-#define SIGMA 3.0
-#define SHIFT 11
+#define SIGMA 5.0
+#define VSHIFT 10
+#define SHIFT 12
 
 
-double src[N];
-double dest[N];
+uint8_t src[N];
+uint8_t dest1[N];
+uint8_t dest2[N];
 
 typedef struct _KernelState KernelState;
 struct _KernelState {
@@ -45,75 +48,21 @@ struct _KernelState {
 };
 
 void
-create_pattern (double *data, int n)
+create_pattern (uint8_t *data, int n)
 {
   int i;
 
   for(i=0;i<n;i++){
     //data[i] = 255*((i>=990) && (i<1010));
     //data[i] = 255*(i==1000);
-    data[i] = 255*((i>>2)&1);
+    data[i] = 255*((i>>3)&1);
+    //data[i] = 100+100*((i>>4)&1);
+    //data[i] = 100;
   }
 }
 
-double
-kernel (KernelState *state, double value)
-{
-  double x;
-
-#if 0
-  x = state->B*value +state->b0inv*
-    (state->y1*state->b1 + state->y2*state->b2 + state->y3*state->b3);
-#endif
-#ifdef INT
-  value *= (1<<state->value_shift);
-  x = (state->d[0]*value + state->d[1]*state->y1 + state->d[2]*state->y2 +
-    state->d[3]*state->y3)/(1<<state->shift);
-  x = rint(x);
-#else
-  x = state->c[0]*value + state->c[1]*state->y1 + state->c[2]*state->y2 +
-    state->c[3]*state->y3;
-#endif
-
-  state->y3 = state->y2;
-  state->y2 = state->y1;
-  state->y1 = x;
-
-  return x/(1<<state->value_shift);
-}
-
-int
-kernel_int (KernelState *state, int value)
-{
-  int x;
-  int y;
-
-  //value <<= state->value_shift;
-  y = state->d[0]*value + state->d[1]*state->y1i + state->d[2]*state->y2i +
-    state->d[3]*state->y3i;
-  //x = (y + (1<<(state->shift - 1)) + state->err) >> state->shift;
-  x = (y + state->err) >> state->shift;
-  state->err = y - (x << state->shift);
-
-  state->y3i = state->y2i;
-  state->y2i = state->y1i;
-  state->y1i = x;
-
-  return (x + (1<<(state->value_shift - 1))) >> state->value_shift;
-}
-
 void
-kernel_reverse (KernelState *state)
-{
-  state->y3 = state->y1;
-  state->y2 = state->y1;
-
-  state->y3i = state->y1i;
-  state->y2i = state->y1i;
-}
-
-void
-kernel_state_init (KernelState *state, double sigma, double value)
+kernel_init (KernelState *state, double sigma)
 {
   double q;
 
@@ -130,97 +79,169 @@ kernel_state_init (KernelState *state, double sigma, double value)
   state->b3 = 0.422205*q*q*q;
   state->B = 1 - (state->b1 + state->b2 + state->b3)/state->b0;
 
-  state->y1 = value;
-  state->y2 = value;
-  state->y3 = value;
-
   state->c[0] = state->B;
   state->c[1] = state->b1 * state->b0inv;
   state->c[2] = state->b2 * state->b0inv;
   state->c[3] = state->b3 * state->b0inv;
 
-  state->value_shift = SHIFT;
+  state->value_shift = VSHIFT;
   state->shift = SHIFT;
   state->d[0] = rint(state->c[0]*(1<<(state->shift + state->value_shift)));
   state->d[1] = rint(state->c[1]*(1<<state->shift));
   state->d[2] = rint(state->c[2]*(1<<state->shift));
   state->d[3] = rint(state->c[3]*(1<<state->shift));
 
-  state->y1i = value;
-  state->y2i = value;
-  state->y3i = value;
   state->err = 0;
 }
 
 void
-test (void)
+kernel_set_start (KernelState *state, int value)
 {
-  double *tmp, *tmpdata;
-  int i;
-  int n = N;
-  KernelState state;
-  double sigma;
+  state->y1 = value;
+  state->y2 = value;
+  state->y3 = value;
 
-  tmpdata = malloc ((N + 8)*sizeof(double));
-  tmp = tmpdata + 4;
+  state->y1i = value << state->value_shift;
+  state->y2i = value << state->value_shift;
+  state->y3i = value << state->value_shift;
+}
 
-  create_pattern (src, N);
+static int
+kernel_int (KernelState *state, int value)
+{
+  int x;
+  int y;
 
-  sigma = SIGMA;
-
-#if 0
-  for(x=1;x<10.0;x+=0.5) {
-  kernel_state_init (&state, x, src[0]);
-  fprintf(stderr,"%g %g %g %g\n", state.c[0], state.c[1], state.c[2], state.c[3]);
-  fprintf(stderr,"%d %d %d %d\n", state.d[0], state.d[1], state.d[2], state.d[3]);
-  }
+  y = state->d[0]*value + state->d[1]*state->y1i + state->d[2]*state->y2i +
+    state->d[3]*state->y3i;
+#if 1
+  x = (y + (1<<(state->shift - 1))) >> state->shift;
+#else
+  x = (y + state->err) >> state->shift;
+  state->err = y - (x << state->shift);
 #endif
-  kernel_state_init (&state, sigma, src[0]);
-  fprintf(stderr,"%g %g %g %g\n", state.c[0], state.c[1], state.c[2], state.c[3]);
-  fprintf(stderr,"%d %d %d %d\n", state.d[0], state.d[1], state.d[2], state.d[3]);
 
+  state->y3i = state->y2i;
+  state->y2i = state->y1i;
+  state->y1i = x;
+
+  return (x + (1<<(state->value_shift - 1))) >> state->value_shift;
+}
+
+void
+filter_u8_int (uint8_t *dest, uint8_t *src, uint8_t *tmp, double sigma, int n)
+{
+  KernelState state;
+  int i;
+
+  kernel_init (&state, sigma);
+
+  kernel_set_start(&state, src[0]);
   for(i=0;i<n;i++) {
-#ifdef INT
     int x;
     x = kernel_int(&state, src[i]);
     if (x < 0) x = 0;
     if (x > 255) x = 255;
-    tmp[i] = rint(x);
-#else
-    double x;
-    x = kernel(&state, src[i]);
     tmp[i] = x;
-#endif
   }
-  kernel_reverse(&state);
+
+  kernel_set_start(&state, tmp[n-1]);
   for(i=n-1;i>=0;i--) {
-#ifdef INT
     int x;
     x = kernel_int(&state, tmp[i]);
     if (x < 0) x = 0;
     if (x > 255) x = 255;
     dest[i] = x;
-#else
-    double x;
-    x = kernel(&state, tmp[i]);
-    dest[i] = x;
-#endif
   }
-
-  for(i=0;i<N;i++){
-    printf("%d %g %g %g %g\n", i, src[i], tmp[i], dest[i],
-        255.0*exp(-(N/2-i)*(N/2-i)/(2*sigma*sigma))/(sqrt(2*M_PI)*sigma));
-  }
-
-  free(tmpdata);
 }
 
-void dump(void)
+static int
+kernel_double (KernelState *state, int value)
+{
+  double x;
+
+  x = state->c[0]*value + state->c[1]*state->y1 + state->c[2]*state->y2 +
+    state->c[3]*state->y3;
+
+  state->y3 = state->y2;
+  state->y2 = state->y1;
+  state->y1 = x;
+
+  return rint(x);
+}
+
+void
+filter_u8_double (uint8_t *dest, uint8_t *src, uint8_t *tmp, double sigma, int n)
+{
+  KernelState state;
+  int i;
+
+  kernel_init (&state, sigma);
+
+  kernel_set_start(&state, src[0]);
+  for(i=0;i<n;i++) {
+    int x;
+    x = kernel_double(&state, src[i]);
+    if (x < 0) x = 0;
+    if (x > 255) x = 255;
+    tmp[i] = x;
+  }
+
+  kernel_set_start(&state, tmp[n-1]);
+  for(i=n-1;i>=0;i--) {
+    int x;
+    x = kernel_double(&state, tmp[i]);
+    if (x < 0) x = 0;
+    if (x > 255) x = 255;
+    dest[i] = x;
+  }
+}
+
+
+
+void
+test (void)
 {
   int i;
-  for(i=0;i<N;i++){
-    printf("%d %g %g\n", i, src[i], dest[i]);
+  double sigma;
+  int diff;
+  OilProfile prof;
+  double ave, std;
+  uint8_t *tmp;
+
+  tmp = malloc (N);
+
+  create_pattern (src, N);
+
+  sigma = SIGMA;
+
+  oil_profile_init (&prof);
+  for(i=0;i<10;i++){
+    oil_profile_start(&prof);
+    filter_u8_double (dest1, src, tmp, sigma, N);
+    oil_profile_stop(&prof);
   }
+  oil_profile_get_ave_std (&prof, &ave, &std);
+  fprintf(stderr, "double cycles %g %g\n", ave, std);
+
+  oil_profile_init (&prof);
+  for(i=0;i<10;i++){
+    oil_profile_start(&prof);
+    filter_u8_int (dest2, src, tmp, sigma, N);
+    oil_profile_stop(&prof);
+  }
+  oil_profile_get_ave_std (&prof, &ave, &std);
+  fprintf(stderr, "int cycles %g %g\n", ave, std);
+
+  diff = 0;
+  for(i=0;i<N;i++){
+    printf("%d %d %d %d\n", i, src[i], dest1[i], dest2[i]);
+    //    255.0*exp(-(N/2-i)*(N/2-i)/(2*sigma*sigma))/(sqrt(2*M_PI)*sigma));
+    diff += abs(dest1[i] - dest2[i]);
+  }
+  fprintf(stderr,"diff %d\n", diff);
+
+  free(tmp);
 }
 
 int
@@ -229,8 +250,6 @@ main (int argc, char *argv[])
   schro_init();
 
   test();
-
-  if(0) dump();
 
   return 0;
 }
