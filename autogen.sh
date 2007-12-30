c9503924831327e14567cd9894cc689e44cd7ccb
David Schleef <ds@ginger.bigkitten.com>
2007-12-29 20:33:02 -0800

    Manual merge from cuda branch

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-autoreconf -i -f &&
-./configure --enable-maintainer-mode --disable-static --enable-gtk-doc $@
+if [ $(whoami) = wladimir ]; then
+  export CFLAGS="-O3 -g"
+  autoreconf -i -f &&
+  ./configure --prefix=/home/wladimir --with-cuda=/usr/local/cuda \
+    --disable-gtk-doc $@
+else
+  autoreconf -i -f &&
+  ./configure --enable-maintainer-mode --disable-static --enable-gtk-doc $@
+fi
+
