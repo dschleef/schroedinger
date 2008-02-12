4276cff3a293679d47abb709841000a0f0839dba
David Schleef <ds@ginger.bigkitten.com>
2008-02-11 16:23:33 -0800

    [core] Revert to rounding strides up to 4, not 64.

--- schroedinger/schroframe.c
+++ schroedinger/schroframe.c
@@ -90,7 +90,7 @@ schro_frame_new_and_alloc (SchroMemoryDomain *domain, SchroFrameFormat format,
 
   frame->components[0].width = width;
   frame->components[0].height = height;
-  frame->components[0].stride = ROUND_UP_64(width * bytes_pp);
+  frame->components[0].stride = ROUND_UP_4(width * bytes_pp);
   frame->components[0].length = 
     frame->components[0].stride * frame->components[0].height;
   frame->components[0].v_shift = 0;
@@ -98,7 +98,7 @@ schro_frame_new_and_alloc (SchroMemoryDomain *domain, SchroFrameFormat format,
 
   frame->components[1].width = chroma_width;
   frame->components[1].height = chroma_height;
-  frame->components[1].stride = ROUND_UP_64(chroma_width * bytes_pp);
+  frame->components[1].stride = ROUND_UP_4(chroma_width * bytes_pp);
   frame->components[1].length = 
     frame->components[1].stride * frame->components[1].height;
   frame->components[1].v_shift = v_shift;
@@ -106,7 +106,7 @@ schro_frame_new_and_alloc (SchroMemoryDomain *domain, SchroFrameFormat format,
 
   frame->components[2].width = chroma_width;
   frame->components[2].height = chroma_height;
-  frame->components[2].stride = ROUND_UP_64(chroma_width * bytes_pp);
+  frame->components[2].stride = ROUND_UP_4(chroma_width * bytes_pp);
   frame->components[2].length = 
     frame->components[2].stride * frame->components[2].height;
   frame->components[2].v_shift = v_shift;
