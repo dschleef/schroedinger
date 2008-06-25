a129cea04ffa3fc36320450b8d18553902bea6c8
David Schleef <ds@schleef.org>
2008-06-25 13:52:06 -0700

    [parse] Add schro_parse_decode_sequence_header to stable API

--- schroedinger/schroparse.h
+++ schroedinger/schroparse.h
@@ -6,12 +6,8 @@
 
 SCHRO_BEGIN_DECLS
 
-#ifdef SCHRO_ENABLE_UNSTABLE_API
-
 int schro_parse_decode_sequence_header (uint8_t *data, int length,
     SchroVideoFormat *video_format);
-
-#endif
 
 SCHRO_END_DECLS
 
