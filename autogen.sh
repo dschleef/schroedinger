4ab2a25256da09b6f082dfc74d0aac70c15679f6
David Schleef <ds@schleef.org>
2008-06-27 16:20:38 -0700

    [build] don't call gtkdocize in autogen.

--- autogen.sh
+++ autogen.sh
@@ -3,11 +3,11 @@
 if [ $(whoami) = wladimir ]; then
   confflags="--prefix=/home/wladimir --with-cuda --disable-gtk-doc"
 elif [ $(whoami) = ds ]; then
-  confflags="--enable-gtk-doc --enable-orc"
+  #confflags="--disable-gtk-doc --enable-orc"
+  confflags="--disable-gtk-doc"
 else
   confflags="--enable-gtk-doc"
 fi
 
 autoreconf -i -f &&
-  gtkdocize --copy &&
   ./configure --enable-maintainer-mode --disable-static $confflags $@
