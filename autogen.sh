2ec7ab09e4fddfd315bde370c8ed9595551b8afc
David Schleef <ds@schleef.org>
2008-12-11 13:25:56 -0800

    [build] If your name is ds, you like to enable extras

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@ if [ $(whoami) = wladimir ]; then
   confflags="--prefix=/home/wladimir --with-cuda --disable-gtk-doc"
 elif [ $(whoami) = ds ]; then
   #confflags="--disable-gtk-doc --enable-orc"
-  confflags="--enable-gtk-doc"
+  confflags="--enable-gtk-doc --enable-extras"
 else
   confflags="--enable-gtk-doc"
 fi
