caa74a0ffb90ac2ef2cca0848cbdf4486c97acfc
David Schleef <ds@schleef.org>
2008-12-13 14:35:14 -0800

    [encoder] Move global motion into new file

---
+++ schroedinger/schroglobalest.c
@@ -0,0 +1,212 @@
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <schroedinger/schro.h>
+#include <liboil/liboil.h>
+#include <string.h>
+#include <math.h>
+
+void schro_motion_global_metric (SchroMotionField *mf, SchroFrame *frame,
+    SchroFrame *ref);
+
+
+void
+schro_encoder_global_estimation (SchroMotionEst *me)
+{
+  SchroParams *params = me->params;
+  SchroMotionField *mf, *mf_orig;
+  int i;
+
+  SCHRO_ERROR("Global prediction is broken.  Please try again later");
+
+  for(i=0;i<params->num_refs;i++) {
+    mf_orig = me->encoder_frame->rme[i]->motion_fields[0];
+    mf = schro_motion_field_new (mf_orig->x_num_blocks, mf_orig->y_num_blocks);
+
+    memcpy (mf->motion_vectors, mf_orig->motion_vectors,
+        sizeof(SchroMotionVector)*mf->x_num_blocks*mf->y_num_blocks);
+    schro_motion_field_global_estimation (mf,
+        &me->encoder_frame->params.global_motion[i],
+        params->mv_precision);
+    if (i == 0) {
+      schro_motion_global_metric (mf,
+          me->encoder_frame->filtered_frame,
+          me->encoder_frame->ref_frame[0]->filtered_frame);
+    } else {
+      schro_motion_global_metric (mf, me->encoder_frame->filtered_frame,
+          me->encoder_frame->ref_frame[1]->filtered_frame);
+    }
+    schro_list_append (me->encoder_frame->motion_field_list, mf);
+  }
+}
+
+void
+schro_motion_global_metric (SchroMotionField *field, SchroFrame *frame,
+    SchroFrame *ref)
+{
+  SchroMotionVector *mv;
+  int i;
+  int j;
+  int x,y;
+
+  for(j=0;j<field->y_num_blocks;j++){
+    for(i=0;i<field->x_num_blocks;i++){
+      mv = field->motion_vectors + j*field->x_num_blocks + i;
+
+      x = i*8 + mv->dx[0];
+      y = j*8 + mv->dy[0];
+#if 0
+      mv->metric = schro_metric_absdiff_u8 (
+            frame->components[0].data + x + y*frame->components[0].stride,
+            frame->components[0].stride,
+            ref->components[0].data + i*8 + j*8*ref->components[0].stride,
+            ref->components[0].stride, 8, 8);
+#endif
+mv->metric = 0;
+    }
+  }
+}
+
+void
+schro_motion_field_global_estimation (SchroMotionField *mf,
+    SchroGlobalMotion *gm, int mv_precision)
+{
+  int i;
+  int j;
+  int k;
+  SchroMotionVector *mv;
+
+  for(j=0;j<mf->y_num_blocks;j++) {
+    for(i=0;i<mf->x_num_blocks;i++) {
+      mv = mf->motion_vectors + j*mf->x_num_blocks + i;
+
+      mv->using_global = 1;
+
+      /* HACK */
+      if (j >= mf->y_num_blocks - 8 || i >= mf->x_num_blocks - 8) {
+        mv->using_global = 0;
+      }
+    }
+  }
+
+  for(k=0;k<4;k++){
+    double m_x, m_y;
+    double m_f, m_g;
+    double pan_x, pan_y;
+    double ave_x, ave_y;
+    double m_fx, m_fy, m_gx, m_gy;
+    double m_xx, m_yy;
+    double a00, a01, a10, a11;
+    double sum2;
+    double stddev2;
+    int n = 0;
+
+    SCHRO_DEBUG("step %d", k);
+    m_x = 0;
+    m_y = 0;
+    m_f = 0;
+    m_g = 0;
+    for(j=0;j<mf->y_num_blocks;j++) {
+      for(i=0;i<mf->x_num_blocks;i++) {
+        mv = mf->motion_vectors + j*mf->x_num_blocks + i;
+        if (mv->using_global) {
+          m_f += mv->dx[0];
+          m_g += mv->dy[0];
+          m_x += i*8;
+          m_y += j*8;
+          n++;
+        }
+      }
+    }
+    pan_x = m_f / n;
+    pan_y = m_g / n;
+    ave_x = m_x / n;
+    ave_y = m_y / n;
+
+    SCHRO_DEBUG("pan %f %f ave %f %f n %d", pan_x, pan_y, ave_x, ave_y, n);
+
+    m_fx = 0;
+    m_fy = 0;
+    m_gx = 0;
+    m_gy = 0;
+    m_xx = 0;
+    m_yy = 0;
+    n = 0;
+    for(j=0;j<mf->y_num_blocks;j++) {
+      for(i=0;i<mf->x_num_blocks;i++) {
+        mv = mf->motion_vectors + j*mf->x_num_blocks + i;
+        if (mv->using_global) {
+          m_fx += (mv->dx[0] - pan_x) * (i*8 - ave_x);
+          m_fy += (mv->dx[0] - pan_x) * (j*8 - ave_y);
+          m_gx += (mv->dy[0] - pan_y) * (i*8 - ave_x);
+          m_gy += (mv->dy[0] - pan_y) * (j*8 - ave_y);
+          m_xx += (i*8 - ave_x) * (i*8 - ave_x);
+          m_yy += (j*8 - ave_y) * (j*8 - ave_y);
+          n++;
+        }
+      }
+    }
+    SCHRO_DEBUG("m_fx %f m_gx %f m_xx %f n %d", m_fx, m_gx, m_xx, n);
+    a00 = m_fx / m_xx;
+    a01 = m_fy / m_yy;
+    a10 = m_gx / m_xx;
+    a11 = m_gy / m_yy;
+
+    pan_x -= a00*ave_x + a01*ave_y;
+    pan_y -= a10*ave_x + a11*ave_y;
+
+    SCHRO_DEBUG("pan %f %f a[] %f %f %f %f", pan_x, pan_y, a00, a01, a10, a11);
+
+    sum2 = 0;
+    for(j=0;j<mf->y_num_blocks;j++) {
+      for(i=0;i<mf->x_num_blocks;i++) {
+        mv = mf->motion_vectors + j*mf->x_num_blocks + i;
+        if (mv->using_global) {
+          double dx, dy;
+          dx = mv->dx[0] - (pan_x + a00 * i + a01 * j);
+          dy = mv->dy[0] - (pan_y + a10 * i + a11 * j);
+          sum2 += dx * dx + dy * dy;
+        }
+      }
+    }
+
+    stddev2 = sum2/n;
+    SCHRO_DEBUG("stddev %f", sqrt(sum2/n));
+
+    if (stddev2 < 1) stddev2 = 1;
+
+    n = 0;
+    for(j=0;j<mf->y_num_blocks;j++) {
+      for(i=0;i<mf->x_num_blocks;i++) {
+        double dx, dy;
+        mv = mf->motion_vectors + j*mf->x_num_blocks + i;
+        dx = mv->dx[0] - (pan_x + a00 * i + a01 * j);
+        dy = mv->dy[0] - (pan_y + a10 * i + a11 * j);
+        mv->using_global = (dx * dx + dy * dy < stddev2*16);
+        n += mv->using_global;
+      }
+    }
+    SCHRO_DEBUG("using n = %d", n);
+
+    gm->b0 = rint(pan_x*(0.125*(1<<mv_precision)));
+    gm->b1 = rint(pan_y*(0.125*(1<<mv_precision)));
+    gm->a_exp = 16;
+    gm->a00 = rint((1.0 + a00/8) * (1<<(gm->a_exp + mv_precision)));
+    gm->a01 = rint(a01/8 * (1<<(gm->a_exp + mv_precision)));
+    gm->a10 = rint(a10/8 * (1<<(gm->a_exp + mv_precision)));
+    gm->a11 = rint((1.0 + a11/8) * (1<<(gm->a_exp + mv_precision)));
+  }
+
+  for(j=0;j<mf->y_num_blocks;j++) {
+    for(i=0;i<mf->x_num_blocks;i++) {
+      mv = mf->motion_vectors + j*mf->x_num_blocks + i;
+      mv->using_global = 1;
+      //mv->dx[0] = gm->b0 + ((gm->a00 * (i*8) + gm->a01 * (j*8))>>gm->a_exp) - i*8;
+      //mv->dy[0] = gm->b1 + ((gm->a10 * (i*8) + gm->a11 * (j*8))>>gm->a_exp) - j*8;
+      mv->dx[0] = 0;
+      mv->dy[0] = 0;
+    }
+  }
+}
+
