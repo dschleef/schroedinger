77bb8717f78a487d76328d8da4cf11c652ea39bb
David Schleef <ds@schleef.org>
2009-03-18 17:42:14 -0700

    build: change my autogen preferences

--- autogen.sh
+++ autogen.sh
@@ -3,8 +3,7 @@
 if [ $(whoami) = wladimir ]; then
   confflags="--prefix=/home/wladimir --with-cuda --disable-gtk-doc"
 elif [ $(whoami) = ds ]; then
-  #confflags="--disable-gtk-doc --enable-orc"
-  confflags="--enable-gtk-doc --enable-extras"
+  confflags="--enable-gtk-doc --enable-orc"
 else
   confflags="--enable-gtk-doc"
 fi
