d808736d37912191fa8a965ab17720274ec1894c
David Schleef <ds@schleef.org>
2009-03-19 23:03:13 -0700

    oil: Remove some disabled code

--- schroedinger/schrooil.c
+++ schroedinger/schrooil.c
@@ -14,68 +14,6 @@ oil_splat_s16_ns (int16_t *dest, const int16_t *src, int n)
   oil_splat_u16_ns ((uint16_t *)dest, (const uint16_t *)src, n);
 }
 
-#if 0
-void
-oil_lift_haar_split (int16_t *i1, int16_t *i2, int n)
-{
-  int i;
-  for(i=0;i<n;i++){
-    i2[i] -= i1[i];
-    i1[i] += (i2[i] + 1)>>1;
-  }
-}
-
-void
-oil_lift_haar_synth (int16_t *i1, int16_t *i2, int n)
-{
-  int i;
-  for(i=0;i<n;i++){
-    i1[i] -= (i2[i] + 1)>>1;
-    i2[i] += i1[i];
-  }
-}
-
-void
-oil_synth_haar (int16_t *d, const int16_t *s, int n)
-{
-  int i;
-
-  for(i=0;i<n;i++){
-    d[2*i] = s[2*i] - ((s[2*i+1] + 1)>>1);
-    d[2*i + 1] = s[2*i+1] + d[2*i];
-  }
-}
-
-void
-oil_split_haar (int16_t *d, const int16_t *s, int n)
-{
-  int i;
-
-  for(i=0;i<n;i++){
-    d[2*i+1] = s[2*i+1] - s[2*i];
-    d[2*i] = s[2*i] + ((d[2*i + 1] + 1)>>1);
-  }
-}
-
-void
-oil_multsumshift8_str_s16 (int16_t *d, const int16_t *s, int sstr,
-    const int16_t *s2_8, const int16_t *s3_1, const int16_t *s4_1, int n)
-{
-  int i;
-  int x;
-  int j;
-
-  for(i=0;i<n;i++){
-    x = s3_1[0];
-    for(j=0;j<8;j++){
-      x += s[j] * s2_8[j];
-    }
-    d[i] = x >> s4_1[0];
-    s = OFFSET(s,sstr);
-  }
-}
-#endif
-
 void
 oil_sum_s32_u8 (int32_t *d_1, uint8_t *src, int n)
 {
@@ -116,59 +54,6 @@ oil_sum_square_diff_u8 (int32_t *d_1, uint8_t *s1, uint8_t *s2, int n)
   d_1[0] = sum;
 }
 
-#if 0
-void
-oil_mas4_u8 (uint8_t *d, const uint8_t *s1_np3, const int16_t *s2_4,
-    const int16_t *s3_2, int n)
-{
-  int i;
-  int j;
-  int x;
-
-  for(i=0;i<n;i++){
-    x = 0;
-    for(j=0;j<4;j++){
-      x += s1_np3[i+j] * s2_4[j];
-    }
-    d[i] = (x + s3_2[0])>>s3_2[1];
-  }
-}
-
-void
-oil_mas4_s16 (int16_t *d, const int16_t *s1_np3, const int32_t *s2_4,
-    const int32_t *s3_2, int n)
-{
-  int i;
-  int j;
-  int x;
-
-  for(i=0;i<n;i++){
-    x = 0;
-    for(j=0;j<4;j++){
-      x += s1_np3[i+j] * s2_4[j];
-    }
-    d[i] = (x + s3_2[0])>>s3_2[1];
-  }
-}
-
-void
-oil_mas8_s16 (int16_t *d, const int16_t *s1_np3, const int32_t *s2_4,
-    const int32_t *s3_2, int n)
-{
-  int i;
-  int j;
-  int x;
-
-  for(i=0;i<n;i++){
-    x = 0;
-    for(j=0;j<8;j++){
-      x += s1_np3[i+j] * s2_4[j];
-    }
-    d[i] = (x + s3_2[0])>>s3_2[1];
-  }
-}
-#endif
-
 void
 oil_mas10_s16 (int16_t *d, const int16_t *s1_np3, const int32_t *s2_4,
     const int32_t *s3_2, int n)
@@ -185,53 +70,6 @@ oil_mas10_s16 (int16_t *d, const int16_t *s1_np3, const int32_t *s2_4,
     d[i] = (x + s3_2[0])>>s3_2[1];
   }
 }
-
-#if 0
-void
-oil_mas8_across_u8 (uint8_t *d, uint8_t **s1_a8,
-    const int16_t *s2_8, const int16_t *s3_2, int n)
-{
-  int i;
-  int j;
-  int x;
-
-  for(i=0;i<n;i++){
-    x = 0;
-    for(j=0;j<8;j++){
-      x += s1_a8[j][i] * s2_8[j];
-    }
-    d[i] = CLAMP((x + s3_2[0])>>s3_2[1],0,255);
-  }
-}
-
-void
-oil_mas10_across_u8 (uint8_t *d, uint8_t **s1_a10,
-    const int16_t *s2_10, const int16_t *s3_2, int n)
-{
-  int i;
-  int j;
-  int x;
-
-  for(i=0;i<n;i++){
-    x = 0;
-    for(j=0;j<10;j++){
-      x += s1_a10[j][i] * s2_10[j];
-    }
-    d[i] = CLAMP((x + s3_2[0])>>s3_2[1],0,255);
-  }
-}
-
-void
-oil_addc_rshift_clipconv_u8_s16 (uint8_t *d1, const int16_t *s1,
-    const int16_t *s2_2, int n)
-{
-  int i;
-
-  for(i=0;i<n;i++){
-    d1[i] = CLAMP((s1[i] + s2_2[0])>>s2_2[1],0,255);
-  }
-}
-#endif
 
 void
 oil_convert_f64_u8 (double *dest, uint8_t *src, int n)
@@ -348,24 +186,5 @@ oil_iir3_rev_u8_f64 (uint8_t *d, uint8_t *s, double *i_3, double *s2_4, int n)
     d[i] = rint(x);
   }
 }
-
-#if 0
-void
-oil_mas12across_addc_rshift_u8 (uint8_t *dest, uint8_t **src,
-    const int16_t *taps, const int16_t *offsetshift, int n)
-{
-  int i;
-  int j;
-  int x;
-
-  for(i=0;i<n;i++){
-    x = 0;
-    for(j=0;j<12;j++){
-      x += taps[j]*src[j][i];
-    }
-    dest[i] = CLAMP((x + offsetshift[0]) >> offsetshift[1],0,255);
-  }
-}
-#endif
 
 
--- schroedinger/schrooil.h
+++ schroedinger/schrooil.h
@@ -10,34 +10,11 @@ SCHRO_BEGIN_DECLS
 #ifdef SCHRO_ENABLE_UNSTABLE_API
 
 void oil_splat_s16_ns (int16_t *dest, const int16_t *src, int n);
-void oil_lift_haar_split (int16_t *i1, int16_t *i2, int n);
-void oil_lift_haar_synth (int16_t *i1, int16_t *i2, int n);
-void oil_synth_haar (int16_t *d, const int16_t *s, int n);
-void oil_split_haar (int16_t *d, const int16_t *s, int n);
-void oil_multsumshift8_str_s16 (int16_t *d, const int16_t *s, int sstr,
-    const int16_t *s2_8, const int16_t *s3_1, const int16_t *s4_1, int n);
 void oil_sum_s32_u8 (int32_t *d_1, uint8_t *src, int n);
 void oil_sum_s32_s16 (int32_t *d_1, int16_t *src, int n);
 void oil_sum_square_diff_u8 (int32_t *d_1, uint8_t *s1, uint8_t *s2, int n);
-void oil_mas4_u8 (uint8_t *d, const uint8_t *s1_np3, const int16_t *s2_4,
-    const int16_t *s3_2, int n);
-void oil_mas4_s16 (int16_t *d, const int16_t *s1_np3, const int32_t *s2_4,
-    const int32_t *s3_2, int n);
-void oil_mas8_s16 (int16_t *d, const int16_t *s1_np3, const int32_t *s2_4,
-    const int32_t *s3_2, int n);
 void oil_mas10_s16 (int16_t *d, const int16_t *s1_np3, const int32_t *s2_4,
     const int32_t *s3_2, int n);
-
-#if 0
-void oil_mas8_across_u8 (uint8_t *d, uint8_t **s1_a8,
-    const int16_t *s2_8, const int16_t *s3_2, int n);
-#endif
-void oil_mas10_across_u8 (uint8_t *d, uint8_t **s1_a10,
-    const int16_t *s2_10, const int16_t *s3_2, int n);
-
-void oil_addc_rshift_clipconv_u8_s16 (uint8_t *d1, const int16_t *s1,
-    const int16_t *s2_2, int n);
-
 void oil_convert_f64_u8 (double *dest, uint8_t *src, int n);
 void oil_iir3_s16_f64 (int16_t *d, int16_t *s, double *i_3, double *s2_4, int n);
 void oil_iir3_rev_s16_f64 (int16_t *d, int16_t *s, double *i_3, double *s2_4, int n);
@@ -46,9 +23,6 @@ void oil_iir3_across_s16_f64 (int16_t *d, int16_t *s, double *i1, double *i2, do
 void oil_convert_f64_s16 (double *dest, int16_t *src, int n);
 void oil_iir3_u8_f64 (uint8_t *d, uint8_t *s, double *i_3, double *s2_4, int n);
 void oil_iir3_rev_u8_f64 (uint8_t *d, uint8_t *s, double *i_3, double *s2_4, int n);
-
-void oil_mas12across_addc_rshift_u8 (uint8_t *dest, uint8_t **src,
-    const int16_t *taps, const int16_t *offsetshift, int n);
 
 #endif
 
