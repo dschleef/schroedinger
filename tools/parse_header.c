a1334cea1b6c116fb5682e975103d7e25eedfd8b
David Schleef <ds@schleef.org>
2009-12-13 19:10:51 -0800

    tools: parse_header search inside containers
    Change parse_header to search the first few kb of the given
    file to find the sequence header.  This make it possible to
    use parse_header on Ogg/Dirac or Dirac-in-MOV files.

--- tools/parse_header.c
+++ tools/parse_header.c
@@ -9,6 +9,10 @@
 #include <string.h>
 #include <errno.h>
 
+#define READ_SIZE 4096
+
+unsigned char data[READ_SIZE];
+
 int parse_packet (FILE *file, unsigned char **p_data, int *p_size);
 
 int
@@ -19,6 +23,8 @@ main (int argc, char *argv[])
   DiracSequenceHeader header = {0};
   int size;
   unsigned char *packet;
+  int n;
+  int i;
 
   if (argc < 2) {
     fprintf(stderr, "parse_header infile.drc\n");
@@ -31,54 +37,64 @@ main (int argc, char *argv[])
     return 1;
   }
 
-  while (1) {
-    ret = parse_packet (file, &packet, &size);
-    if (!ret) {
-      printf("parse error\n");
-      exit(1);
-    }
-    if (packet == NULL) break;
-    
-    if (SCHRO_PARSE_CODE_IS_SEQ_HEADER(packet[4])) {
-      ret = dirac_sequence_header_parse (&header, packet + 13, size - 13);
-      
-      if (!ret) {
-        printf("bad header\n");
-        exit(1);
+  n = fread (data, 1, 4096, file);
+  if (n < 13) {
+    printf("file too short\n");
+    exit(1);
+  }
+
+  for(i=0;i<=n - 13;i++){
+    packet = data + i;
+    if (packet[0] == 'B' && packet[1] == 'B' && packet[2] == 'C' &&
+        packet[3] == 'D') {
+      size = (packet[5]<<24) | (packet[6]<<16) | (packet[7]<<8) | (packet[8]);
+      if (size == 0) {
+        size = 13;
       }
 
+      if (i + size > n) {
+        continue;
+      }
+
+      if (SCHRO_PARSE_CODE_IS_SEQ_HEADER(packet[4])) {
+        ret = dirac_sequence_header_parse (&header, packet + 13, size - 13);
+        
+        if (!ret) {
+          printf("bad header\n");
+          exit(1);
+        }
+
 #define PRINT(ack) printf( #ack ": %d\n", header. ack );
-      PRINT(major_version);
-      PRINT(minor_version);
-      PRINT(profile);
-      PRINT(level);
-      PRINT(index);
-      PRINT(width);
-      PRINT(height);
-      PRINT(chroma_format);
-      PRINT(interlaced);
-      PRINT(top_field_first);
-      PRINT(frame_rate_numerator);
-      PRINT(frame_rate_denominator);
-      PRINT(aspect_ratio_numerator);
-      PRINT(aspect_ratio_denominator);
-      PRINT(clean_width);
-      PRINT(clean_height);
-      PRINT(left_offset);
-      PRINT(top_offset);
-      PRINT(luma_offset);
-      PRINT(luma_excursion);
-      PRINT(chroma_offset);
-      PRINT(chroma_excursion);
-      PRINT(colour_primaries);
-      PRINT(colour_matrix);
-      PRINT(transfer_function);
-      PRINT(interlaced_coding);
+        PRINT(major_version);
+        PRINT(minor_version);
+        PRINT(profile);
+        PRINT(level);
+        PRINT(index);
+        PRINT(width);
+        PRINT(height);
+        PRINT(chroma_format);
+        PRINT(interlaced);
+        PRINT(top_field_first);
+        PRINT(frame_rate_numerator);
+        PRINT(frame_rate_denominator);
+        PRINT(aspect_ratio_numerator);
+        PRINT(aspect_ratio_denominator);
+        PRINT(clean_width);
+        PRINT(clean_height);
+        PRINT(left_offset);
+        PRINT(top_offset);
+        PRINT(luma_offset);
+        PRINT(luma_excursion);
+        PRINT(chroma_offset);
+        PRINT(chroma_excursion);
+        PRINT(colour_primaries);
+        PRINT(colour_matrix);
+        PRINT(transfer_function);
+        PRINT(interlaced_coding);
 
-      exit(0);
+        return 0;
+      }
     }
-
-    free(packet);
   }
 
   return 0;
